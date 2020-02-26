2bc34cf59bb6dfafa98fc5a6e8f9560eb430582f
andrea <andrea.fiori.1998@gmail.com>
2020-02-26 10:57:41 +0000

    aggiunge javadoc Recuperatore

--- src/domotix/controller/Recuperatore.java
+++ src/domotix/controller/Recuperatore.java
@@ -59,6 +59,11 @@ public class Recuperatore {
         return ElencoCategorieAttuatori.getInstance().getCategoria(nomeCategoria);
     }
 
+    /**
+     * Metodo di recupero di una lista di nomi delle unita' immobiliari
+     * presenti all'interno del model.
+     * @return Array di nomi delle unita' immobiliari esistenti
+     */
     public static String[] getNomiUnita() {
         return ElencoUnitaImmobiliari.getInstance().getUnita()
                 .stream()
@@ -66,6 +71,11 @@ public class Recuperatore {
                 .toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero di una lista di descrizioni delle unita' immobiliari
+     * presenti all'interno del model.
+     * @return Array di descrizioni delle unita' immobiliari presenti
+     */
     public static String[] getDescrizioniUnita() {
         return ElencoUnitaImmobiliari.getInstance().getUnita()
                 .stream()
@@ -73,10 +83,25 @@ public class Recuperatore {
                 .toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero di una lista di nomi di tutte le stanze
+     * (compresa quella di default) presenti all'interno di un'unita' immobiliare.
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return Array di nomi delle stanze esistenti
+     */
     public static String[] getNomiStanze(String nomeUnita) {
         return getNomiStanze(nomeUnita, true);
     }
 
+    /**
+     * Metodo di recupero di una lista di nomi delle stanze
+     * presenti all'interno di un'unita' immobiliare.
+     * Se elencaStanzaDefault e' false, non verra' recuperato il nome della stanza
+     * di default.
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @param elencaStanzaDefault Flag che specifica se recuperare il nome della stanza di default o meno
+     * @return Array di nomi delle stanze esistenti
+     */
     public static String[] getNomiStanze(String nomeUnita, boolean elencaStanzaDefault) {
         UnitaImmobiliare unita = getUnita(nomeUnita);
         return Stream.of(unita.getStanze()).map(s -> s.getNome()).filter(s -> elencaStanzaDefault || !s.equals(UnitaImmobiliare.NOME_STANZA_DEFAULT)).toArray(String[]::new);
@@ -90,27 +115,64 @@ public class Recuperatore {
         return Stream.of(sistema.getSensori()).map(s -> s.getNome()).toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero dei nomi degli attuatori all'interno di una stanza.
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return Array di nomi di attuatori
+     */
     public static String[] getNomiAttuatori(String nomeStanza, String nomeUnita) {
         return getNomiAttuatoriSistema(getStanza(nomeStanza, nomeUnita));
     }
 
+    /**
+     * Metodo di recupero dei nomi dei sensori all'interno di una stanza.
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'unita' selezionata
+     * @return Array di nomi di sensori
+     */
     public static String[] getNomiSensori(String nomeStanza, String nomeUnita) {
         return getNomiSensoriSistema(getStanza(nomeStanza, nomeUnita));
     }
 
+    /**
+     * Metodo di recupero dei nomi degli attuatori all'interno di un artefatto.
+     * @param nomeArtefatto Nome dell'artefatto selezionato
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'unita' selezionata
+     * @return Array di nomi di attuatori
+     */
     public static String[] getNomiAttuatori(String nomeArtefatto, String nomeStanza, String nomeUnita) {
         return getNomiAttuatoriSistema(getArtefatto(nomeArtefatto, nomeStanza, nomeUnita));
     }
 
+    /**
+     * Metodo di recupero dei nomi dei sensori all'interno di un artefatto.
+     * @param nomeArtefatto Nome dell'artefatto selezionato
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'unita' selezionata
+     * @return Array di nomi di sensori
+     */
     public static String[] getNomiSensori(String nomeArtefatto, String nomeStanza, String nomeUnita) {
         return getNomiSensoriSistema(getArtefatto(nomeArtefatto, nomeStanza, nomeUnita));
     }
 
+    /**
+     * Metodo di recupero dei nomi degli artefatti all'interno di una stanza
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return Array di nomi di artefatti
+     */
     public static String[] getNomiArtefatti(String nomeStanza, String nomeUnita) {
         Artefatto[] artefatti = getStanza(nomeStanza, nomeUnita).getArtefatti();
         return Stream.of(artefatti).map(a -> a.getNome()).toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero dei nomi delle categorie di sensori all'interno
+     * del model.
+     * @return Array di nomi di categorie di sensori
+     */
     public static String[] getNomiCategorieSensori() {
         return ElencoCategorieSensori.getInstance().getCategorie()
                 .stream()
@@ -118,6 +180,11 @@ public class Recuperatore {
                 .toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero dei nomi delle categorie di attuatori all'interno
+     * del model.
+     * @return Array di nomi di categorie di attuatori
+     */
     public static String[] getNomiCategorieAttuatori() {
         return ElencoCategorieAttuatori.getInstance().getCategorie()
                 .stream()
@@ -125,6 +192,11 @@ public class Recuperatore {
                 .toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero delle descrizioni delle categorie di sensori
+     * all'interno del model.
+     * @return Array di descrizioni di categorie di sensori
+     */
     public static String[] getDescrizioniCategorieSensori() {
         return ElencoCategorieSensori.getInstance().getCategorie()
                 .stream()
@@ -132,6 +204,11 @@ public class Recuperatore {
                 .toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero delle descrizioni delle categorie di attuatori
+     * all'interno del model.
+     * @return Array di descrizioni di categorie di attuatori
+     */
     public static String[] getDescrizioniCategorieAttuatori() {
         return ElencoCategorieAttuatori.getInstance().getCategorie()
                 .stream()
@@ -139,14 +216,35 @@ public class Recuperatore {
                 .toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero della descrizione di un'unita' immobiliare
+     * all'interno del model.
+     * @param nomeUnita Nome dell'untia' immobiliare
+     * @return Descrizione dell'unita' immobiliare
+     */
     public static String getDescrizioneUnita(String nomeUnita) {
         return getUnita(nomeUnita).toString();
     }
 
+    /**
+     * Metodo di recupero della descrizione di una stanza
+     * all'interno di un'unita' immobiliare specificata.
+     * @param nomeStanza Nome della stanza
+     * @param nomeUnita Nome dell'untia' immobiliare selezionata
+     * @return Descrizione della stanza
+     */
     public static String getDescrizioneStanza(String nomeStanza, String nomeUnita) {
         return getStanza(nomeStanza, nomeUnita).toString();
     }
 
+    /**
+     * Metodo di recupero della descrizione di un artefatto
+     * all'interno di una stanza specificata.
+     * @param nomeArtefatto Nome dell'artefatto
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'untia' immobiliare selezionata
+     * @return Descrizione della stanza
+     */
     public static String getDescrizioneArtefatto(String nomeArtefatto, String nomeStanza, String nomeUnita) {
         return getArtefatto(nomeArtefatto, nomeStanza, nomeUnita).toString();
     }
@@ -167,18 +265,52 @@ public class Recuperatore {
                 .toArray(String[]::new);
     }
 
+    /**
+     * Metodo di recupero dei nomi dei sensori gia' esistenti che possono essere
+     * condivisi con la stanza specificata. In sostanza: il metodo recupera tutti i sensori
+     * con una categoria di sensore che non e' gia' presente all'interno della stanza.
+     * @param nomeStanza Nome della stanza
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return Array di nomi di sensori condivisibili con la stanza
+     */
     public static String[] getNomiSensoriAggiungibiliStanza(String nomeStanza, String nomeUnita) {
         return getNomiSensoriAggiungibiliSistema(getStanza(nomeStanza, nomeUnita), nomeUnita);
     }
 
+    /**
+     * Metodo di recupero dei nomi dei sensori gia' esistenti che possono essere
+     * condivisi con l'artefatto specificato. In sostanza: il metodo recupera tutti i sensori
+     * con una categoria di sensore che non e' gia' presente all'interno dell'artefatto.
+     * @param nomeArtefatto Nome dell'artefatto
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return Array di nomi di sensori condivisibili con l'artefatto
+     */
     public static String[] getNomiSensoriAggiungibiliArtefatto(String nomeArtefatto, String nomeStanza, String nomeUnita) {
         return getNomiSensoriAggiungibiliSistema(getArtefatto(nomeArtefatto, nomeStanza, nomeUnita), nomeUnita);
     }
 
+    /**
+     * Metodo di recupero dei nomi degli attuatori gia' esistenti che possono essere
+     * condivisi con la stanza specificata. In sostanza: il metodo recupera tutti gli attuatori
+     * con una categoria di attuatore che non e' gia' presente all'interno della stanza.
+     * @param nomeStanza Nome della stanza
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return Array di nomi di attuatori condivisibili con la stanza
+     */
     public static String[] getNomiAttuatoriAggiungibiliStanza(String nomeStanza, String nomeUnita) {
         return getNomiAttuatoriAggiungibiliSistema(getStanza(nomeStanza, nomeUnita), nomeUnita);
     }
 
+    /**
+     * Metodo di recupero dei nomi degli attuatori gia' esistenti che possono essere
+     * condivisi con l'artefatto specificato. In sostanza: il metodo recupera tutti gli attuatori
+     * con una categoria di attuatore che non e' gia' presente all'interno dell'artefatto.
+     * @param nomeArtefatto Nome dell'artefatto
+     * @param nomeStanza Nome della stanza selezionata
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return Array di nomi di attuatori condivisibili con l'artefatto
+     */
     public static String[] getNomiAttuatoriAggiungibiliArtefatto(String nomeArtefatto, String nomeStanza, String nomeUnita) {
         return getNomiAttuatoriAggiungibiliSistema(getArtefatto(nomeArtefatto, nomeStanza, nomeUnita), nomeUnita);
     }
