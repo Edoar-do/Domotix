e701c54e1ce25917118b0db89040d0ff35976e2d
andrea <andrea.fiori.1998@gmail.com>
2020-02-26 10:22:23 +0000

    aggiunge javadoc a Verificatore

--- src/domotix/controller/Verificatore.java
+++ src/domotix/controller/Verificatore.java
@@ -20,20 +20,45 @@ public class Verificatore {
         return nome.matches("^(" + REGEX_NOMI_DISPOSITIVI + ")$");
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * una ModalitaOperativa all'interno del Model siano valide
+     * @param nome Nome della modalita' operativa
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaModalitaOperativa(String nome) {
         return isNomeValido(nome);
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * una CategoriaSensore all'interno del Model siano valide
+     * @param nome Nome della categoria di sensore
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaCategoriaSensore(String nome) {
         return isNomeValido(nome) &&
                 !ElencoCategorieSensori.getInstance().contains(nome);
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * una CategoriaAttuatore all'interno del Model siano valide
+     * @param nome Nome della categoria di attuatore
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaCategoriaAttuatore(String nome) {
         return isNomeValido(nome) &&
                 !ElencoCategorieAttuatori.getInstance().contains(nome);
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * una Stanza all'interno del Model siano valide
+     * @param nomeStanza Nome della stanza
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaStanza(String nomeStanza, String nomeUnita) {
         return isNomeValido(nomeStanza) &&
                 !nomeStanza.equals(UnitaImmobiliare.NOME_STANZA_DEFAULT) &&
@@ -41,6 +66,13 @@ public class Verificatore {
                 Recuperatore.getStanza(nomeStanza, nomeUnita) == null;
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * un Artefatto all'interno del Model siano valide
+     * @param nomeArtefatto Nome dell'artefatto
+     * @param nomeUnita Nome dell'unita' immobiliare selezionata
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaArtefatto(String nomeArtefatto, String nomeUnita) {
         if (!isNomeValido(nomeArtefatto)) return false;
         if (Recuperatore.getUnita(nomeUnita) == null) return false;
@@ -52,6 +84,11 @@ public class Verificatore {
         return true;
     }
 
+    /**
+     * Verifica che il sensore specificato sia univoco a livello di Model
+     * @param nomeSensore Nome del sensore
+     * @return true se il sensore e' univoco
+     */
     public static boolean checkUnivocitaSensore(String nomeSensore) {
         for (Sensore s : ElencoSensori.getInstance().getDispositivi()) {
             if (s.getNome().equals(nomeSensore)) return false;
@@ -59,6 +96,11 @@ public class Verificatore {
         return true;
     }
 
+    /**
+     * Verifica che l'attuatore specificato sia univoco a livello di Model
+     * @param nomeAttuatore Nome dell'attuatore
+     * @return true se l'attuatore e' univoco
+     */
     public static boolean checkUnivocitaAttuatore(String nomeAttuatore) {
         for (Attuatore a : ElencoAttuatori.getInstance().getDispositivi()) {
             if (a.getNome().equals(nomeAttuatore)) return false;
@@ -66,6 +108,15 @@ public class Verificatore {
         return true;
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * un Sensore all'interno di una Stanza siano valide
+     * @param nomeComposto Nome del sensore
+     * @param nomeCategoria Nome della categoria del sensore
+     * @param nomeStanza Nome della stanza a cui appartiene il sensore
+     * @param nomeUnita Nome dell'unita' immobiliare a cui appartiene il sensore
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaSensore(String nomeComposto, String nomeCategoria, String nomeStanza, String nomeUnita) {
         return isNomeDispositivoValido(nomeComposto) &&
                 checkUnivocitaSensore(nomeComposto) &&
@@ -74,6 +125,15 @@ public class Verificatore {
                 !Recuperatore.getStanza(nomeStanza, nomeUnita).contieneCategoriaSensore(nomeCategoria);
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * un Attuatore all'interno di una Stanza siano valide
+     * @param nomeComposto Nome dell'attuatore
+     * @param nomeCategoria Nome della categoria dell'attuatore
+     * @param nomeStanza Nome della stanza a cui appartiene l'attuatore
+     * @param nomeUnita Nome dell'unita' immobiliare a cui appartiene l'attuatore
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaAttuatore(String nomeComposto, String nomeCategoria, String nomeStanza, String nomeUnita) {
         return isNomeDispositivoValido(nomeComposto) &&
                 checkUnivocitaAttuatore(nomeComposto) &&
@@ -82,6 +142,16 @@ public class Verificatore {
                 !Recuperatore.getStanza(nomeStanza, nomeUnita).contieneCategoriaAttuatore(nomeCategoria);
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * un Sensore all'interno di un Artefatto siano valide
+     * @param nomeComposto Nome del sensore
+     * @param nomeCategoria Nome della categoria del sensore
+     * @param nomeArtefatto Nome dell'artefatto a cui appartiene il sensore
+     * @param nomeStanza Nome della stanza a cui appartiene il sensore
+     * @param nomeUnita Nome dell'unita' immobiliare a cui appartiene il sensore
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaSensore(String nomeComposto, String nomeCategoria, String nomeArtefatto, String nomeStanza, String nomeUnita) {
         return isNomeDispositivoValido(nomeComposto) &&
                 checkUnivocitaSensore(nomeComposto) &&
@@ -91,6 +161,16 @@ public class Verificatore {
                 !Recuperatore.getArtefatto(nomeArtefatto, nomeStanza, nomeUnita).contieneCategoriaSensore(nomeCategoria);
     }
 
+    /**
+     * Verifica che le informazioni fornite per la costruzione e l'aggiunta di
+     * un Attuatore all'interno di un Artefatto siano valide
+     * @param nomeComposto Nome del sensore
+     * @param nomeCategoria Nome della categoria dell'attuatore
+     * @param nomeArtefatto Nome dell'artefatto a cui appartiene l'attuatore
+     * @param nomeStanza Nome della stanza a cui appartiene l'attuatore
+     * @param nomeUnita Nome dell'unita' immobiliare a cui appartiene l'attuatore
+     * @return true se le informazioni sono valide
+     */
     public static boolean checkValiditaAttuatore(String nomeComposto, String nomeCategoria, String nomeArtefatto, String nomeStanza, String nomeUnita) {
         return isNomeDispositivoValido(nomeComposto) &&
                 checkUnivocitaAttuatore(nomeComposto) &&
