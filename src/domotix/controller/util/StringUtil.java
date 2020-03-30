272fd4d18e1d106704f69bb09b97ebbf1f34dabf
andrea <andrea.fiori.1998@gmail.com>
2020-03-30 13:56:09 +0100

    aggiunge LocalDateTime a package regole

--- src/domotix/controller/util/StringUtil.java
+++ src/domotix/controller/util/StringUtil.java
@@ -86,10 +86,4 @@ public class StringUtil {
 
         return ns;
     }
-
-    public static String timestr(int mintime) {
-        int hour = mintime / 60;
-        int minutes = mintime - hour * 60;
-        return hour + "." + minutes;
-    }
 }
