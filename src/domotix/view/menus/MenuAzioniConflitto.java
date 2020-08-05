02ce7e0fec72c2225b9f1723cd521867d8b3cbb8
Edoar_do <edokoppola@gmail.com>
2020-08-05 17:20:12 +0200

    Grafica menu azioni conflitto

--- src/domotix/view/menus/MenuAzioniConflitto.java
+++ src/domotix/view/menus/MenuAzioniConflitto.java
@@ -6,6 +6,7 @@ import domotix.controller.Recuperatore;
 import domotix.view.InputDati;
 import domotix.view.MyMenu;
 
+import javax.swing.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,14 +18,12 @@ import java.util.List;
  * @author paolopasqua
  */
 public class MenuAzioniConflitto {
-    private static final String NESSUNA = "Nessuna delle presenti";
-    private static final String TITOLO_ELIMINA = "Seleziona quale azione ignorare ed eliminare: ";
-    private static final String TITOLO_ESEGUI = "Seleziona quale azione eseguire immediatamente: ";
 
-    private static final String AVVISA_PROSEGUIMENTO_DOPO_ELIMINA = "Verranno ora presentate le azioni rimenenti per decidere quale eseguire subito. Premere invio per procedere...";
-    private static final String AVVISA_PROSEGUIMENTO_DOPO_ESEGUI = "Tutte le azioni rimanenti saranno riprogrammate per l'esecuzione. Premere invio per procedere...";
+    private static final String TITOLO_ELIMINA = "Seleziona quali azioni ignorare ed eliminare";
+    private static final String TITOLO_ESEGUI = "Seleziona quali azioni eseguire immediatamente";
 
-    private MyMenu menu = null;
+    private static final String AVVISA_PROSEGUIMENTO_DOPO_ESEGUI = "Tutte le azioni rimanenti saranno riprogrammate per l'esecuzione";
+
     private Rappresentatore rappresentatore = null;
     private Recuperatore recuperatore = null;
     private Modificatore modificatore = null;
@@ -48,60 +47,70 @@ public class MenuAzioniConflitto {
         List<String> idAzioni = this.recuperatore.getIdAzioniProgrammate();
         List<String> descrizioniAzione = new ArrayList<>();
 
-        if (!idAzioni.isEmpty()) {
-            this.menu = new MyMenu("Azioni in conflitto", null);
-
+        if (!idAzioni.isEmpty()) { //se ci sono azioni programmate
             //recupero le descrizioni delle azioni
             for (int i = 0; i < idAzioni.size(); i++) {
                 descrizioniAzione.add(i, this.rappresentatore.getDescrizioneAzioneProgrammata(idAzioni.get(i)));
             }
 
-            premenuElimina(idAzioni, descrizioniAzione);
-
-            if (idAzioni.size() > 0) {
-                InputDati.leggiStringa(AVVISA_PROSEGUIMENTO_DOPO_ELIMINA);
-
-                premenuEsegui(idAzioni, descrizioniAzione);
-            }
+            premenuElimina(idAzioni, descrizioniAzione); //FATTO
 
             if (idAzioni.size() > 0)
-                InputDati.leggiStringa(AVVISA_PROSEGUIMENTO_DOPO_ESEGUI);
+                premenuEsegui(idAzioni, descrizioniAzione);//FATTO
+
+            if (idAzioni.size() > 0)
+                JOptionPane.showOptionDialog(null, AVVISA_PROSEGUIMENTO_DOPO_ESEGUI, "Riprogrammazione Azioni rimaste", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Ok"}, null);
+
         }
     }
 
     private void premenuElimina(List<String> idAzioni, List<String> descrizioniAzioni) {
-        int sceltaMenu = 0;
 
-        do {
-            this.menu.setSottotitolo(TITOLO_ELIMINA);
-            this.menu.setVoci(descrizioniAzioni.toArray(new String[0]));
-            sceltaMenu = menu.scegli(NESSUNA);
+        DefaultListModel azioniModel = new DefaultListModel();
+        for (String d : descrizioniAzioni) { azioniModel.addElement(d); }
+        JList<String> jListDescrizioniAzioni = new JList<>(azioniModel);
+        JScrollPane scrollPane = new JScrollPane(jListDescrizioniAzioni);
 
-            if (sceltaMenu > 0) {
-                String id = idAzioni.get(sceltaMenu-1); //recupero l'id
-                //elimino l'azione da rimuovere dagli elenchi
-                idAzioni.remove(sceltaMenu-1);
-                descrizioniAzioni.remove(sceltaMenu-1);
-                this.modificatore.rimuoviAzioneProgrammata(id, false); //rimuovo l'azione dai dati
-            }
-        }while(sceltaMenu != 0 && idAzioni.size() > 0);
+        Object[] widgets = new Object[]{
+                "Lista delle descrizioni delle azioni programmate",
+                scrollPane,
+                (TITOLO_ELIMINA + ". Poi premi il bottone 'Elimina' ")
+        };
+
+        JOptionPane.showOptionDialog(null, widgets, "Eliminazione Azioni Programmate", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, new String[]{"Elimina"}, null);
+
+        int[] indiciSelezionati = jListDescrizioniAzioni.getSelectedIndices();
+
+        for (int i = indiciSelezionati.length-1; i >= 0; i--){
+            String id = idAzioni.get(indiciSelezionati[i]);
+            this.modificatore.rimuoviAzioneProgrammata(id, false);
+            idAzioni.remove(indiciSelezionati[i]);
+            azioniModel.remove(indiciSelezionati[i]);
+        }
     }
 
     private void premenuEsegui(List<String> idAzioni, List<String> descrizioniAzioni) {
-        int sceltaMenu = 0;
 
-        do {
-            this.menu.setSottotitolo(TITOLO_ESEGUI);
-            this.menu.setVoci(descrizioniAzioni.toArray(new String[0]));
-            sceltaMenu = menu.scegli(NESSUNA);
+        DefaultListModel azioniModel = new DefaultListModel();
+        for (String d : descrizioniAzioni) { azioniModel.addElement(d); }
+        JList<String> jListDescrizioniAzioni = new JList<>(azioniModel);
+        JScrollPane scrollPane = new JScrollPane(jListDescrizioniAzioni);
 
-            if (sceltaMenu > 0) {
-                String id = idAzioni.get(sceltaMenu-1); //recupero l'id
-                //elimino l'azione da rimuovere dagli elenchi
-                idAzioni.remove(sceltaMenu-1);
-                descrizioniAzioni.remove(sceltaMenu-1);
-                this.modificatore.rimuoviAzioneProgrammata(id, true); //rimuovo l'azione dai dati eseguendola
-            }
-        }while(sceltaMenu != 0 && idAzioni.size() > 0);
+        Object[] widgets = new Object[]{
+                "Lista delle descrizioni delle azioni programmate",
+                scrollPane,
+                (TITOLO_ESEGUI + ". Poi premi il bottone 'Esegui' ")
+        };
+
+        JOptionPane.showOptionDialog(null, widgets, "Esecuzione Azioni Programmate", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, new String[]{"Esegui"}, null);
+
+        int[] indiciSelezionati = jListDescrizioniAzioni.getSelectedIndices();
+
+        for (int i = indiciSelezionati.length-1; i >= 0; i--){
+            String id = idAzioni.get(indiciSelezionati[i]);
+            this.modificatore.rimuoviAzioneProgrammata(id, true);
+            idAzioni.remove(indiciSelezionati[i]);
+            azioniModel.remove(indiciSelezionati[i]);
+        }
     }
 }
